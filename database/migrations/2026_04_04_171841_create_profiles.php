733a550af289a116090e33cadf6155fe9376f05e
Jahid Rahman <rahmanjahid645@gmail.com>
2026-04-06 00:40:50 +0600

    migration(profile)

---
+++ database/migrations/2026_04_04_171841_create_profiles.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName', 50);
+            $table->string('lastName', 50);
+            $table->string('phone', 20);
+            $table->string('address', 255);
+            $table->string('shippingAddress', 255);
+            $table->string('profile_email', 50)->unique();
+            // $table->timestamps();
+
+            // Relattionship
+            $table->foreign('profile_email')->references('email')->on('users')->restrictOnDelete()->cascadeOnUpdate();
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamps('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
